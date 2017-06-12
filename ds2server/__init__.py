95d739d7a30dfe2f8ae7436e948cc16832e1fe94
Oliver Nagy <olitheolix@gmail.com>
2017-06-12 11:45:58 +1000

    Removed Cookiecutter boilerplate.

--- ds2server/__init__.py
+++ ds2server/__init__.py
@@ -1,14 +1,1 @@
 # -*- coding: utf-8 -*-
-
-__author__ = """Oliver Nagy"""
-__email__ = 'olitheolix@gmail.com'
-__version__ = '0.1.0'
-
-import os
-import sys
-
-# Path to Cython generated libraries, and Protocol buffers.
-fpath = os.path.dirname(os.path.abspath(__file__))
-libpath = os.path.join(fpath, '..', 'lib')
-sys.path.insert(0, libpath)
-del fpath, libpath
