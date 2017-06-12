2aa42352289b057eb7f6606380d50c805d90c08b
Oliver Nagy <olitheolix@gmail.com>
2017-06-13 09:51:57 +1000

    Dummy renderer now plays pre-recorded image sequence.

--- ds2server/rendering.py
+++ ds2server/rendering.py
@@ -1,7 +1,5 @@
-import io
 import os
-import numpy as np
-from PIL import Image
+import glob
 
 try:
     import ds2server.horde
@@ -22,18 +20,14 @@ def getEngine(disable_horde=False):
 
 class DummyHorde:
     def __init__(self):
-        src_path = os.path.dirname(os.path.abspath(__file__))
-        self.src_path = os.path.join(src_path, '..', 'dataset')
+        img_path = os.path.dirname(os.path.abspath(__file__))
+        img_path = os.path.join(img_path, 'pre-rendered')
+        fnames = glob.glob(os.path.join(img_path, '*.jpg'))
+        fnames.sort()
+        self.fnames = fnames
+        self.frame_cnt = 0
 
     def renderScene(self, cmat, width, height):
-        img = np.zeros((width, height, 3), np.uint8)
-        x0, x1 = sorted(np.random.randint(0, width, 2).tolist())
-        y0, y1 = sorted(np.random.randint(0, height, 2).tolist())
-        for i in range(3):
-            img[y0:y1, x0:x1, i] = np.random.randint(0, 255)
-        img = Image.fromarray(img)
-
-        f = io.BytesIO()
-        img.save(f, format='jpeg')
-        f.seek(0)
-        return f.read()
+        fname = self.fnames[self.frame_cnt]
+        self.frame_cnt = (self.frame_cnt + 1) % len(self.fnames)
+        return open(fname, 'rb').read()
