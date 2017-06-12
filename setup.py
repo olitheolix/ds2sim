92d440e057616efbada449f9a36499097c890e73
Oliver Nagy <olitheolix@gmail.com>
2017-06-12 12:40:20 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'matplotlib',
+    'pillow',
+    'requests',
+    'tornado',
+]
+
+test_requirements = [
+    'pytest'
+]
+
+setup(
+    name='ds2server',
+    version='0.1.0',
+    description="Fun with Machine Learning and Space Ships",
+    long_description=readme + '\n\n',
+    author="Oliver Nagy",
+    author_email='olitheolix@gmail.com',
+    url='https://github.com/olitheolix/ds2server',
+    packages=['ds2server'],
+    include_package_data=True,
+    zip_safe=False,
+    license="Apache Software License 2.0",
+    keywords='ds2server',
+    test_suite='tests',
+    install_requires=requirements,
+    tests_require=test_requirements,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
