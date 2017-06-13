a34527a63f01563fcff07ce220c0835760e965b4
Oliver Nagy <olitheolix@gmail.com>
2017-06-13 14:12:38 +1000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='ds2server',
-    version='0.1.0',
+    version='0.2.0',
     description="Fun with Machine Learning and Space Ships",
     long_description=readme + '\n\n',
     author="Oliver Nagy",
